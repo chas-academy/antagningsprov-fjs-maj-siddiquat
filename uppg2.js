b8838b1ba4ce1907d68bdc3b1a0d4083573ec360
siddiquat9@gmail.com <156226356+siddiquat@users.noreply.github.com>
2025-05-29 15:58:31 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -2,10 +2,9 @@
 function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
-<script>
    let namn = "Tanzila";
-   console.log(Tanzila);
-   </script>
+   console. log(Tanzila);
+   
 }
 
 module.exports = { uppg2 };
